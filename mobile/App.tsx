ad62d53d17453432868184f09a7d38260c6104f3
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-17 09:44:56 -0300

    Class 01 - Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,48 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/inter'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { Loading } from './src/components/Loading'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#f9f9fa',
+    fontFamily: 'Inter_600SemiBold',
+  },
+})

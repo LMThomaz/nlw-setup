4f182692eecf7340521fe4b06be1611918f0637f
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-18 11:24:30 -0300

    Class 02 - Web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
-import { Habit } from './components/Habit'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={1} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App

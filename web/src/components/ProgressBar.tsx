0694252652ca97274bebeae88b90c49444638bfa
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-19 09:48:23 -0300

    Class 03 - Web

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,23 @@
+import { CSSProperties } from 'react'
+
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  const progressStyles: CSSProperties = {
+    width: `${progress}%`,
+  }
+
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        className="h-3 rounded-lg bg-violet-600"
+        style={progressStyles}
+      />
+    </div>
+  )
+}

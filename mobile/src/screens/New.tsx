78e9d2c074ebe3cfcad50c72cbefc1920fb6b70a
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-22 10:06:02 -0300

    Class 05 - Mobile

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -40,11 +40,10 @@ export function New() {
   async function handleCreateNewHabit() {
     try {
       if (!title.trim() || weekDays.length === 0) {
-        Alert.alert(
+        return Alert.alert(
           'Novo Hábito',
           'Informe o nome do hábito e escolha a periodicidade',
         )
-        return
       }
 
       await api.post('/habits', {
